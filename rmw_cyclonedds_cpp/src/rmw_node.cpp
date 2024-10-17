46a3ad9b7d4544151310c81ad629e4cc3222bf5f
Tomoya Fujita <Tomoya.Fujita@sony.com>
2024-10-17 14:02:32 -0700

    add a case for RMW_EVENT_TYPE_MAX to rmw_take_event().
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- rmw_cyclonedds_cpp/src/rmw_node.cpp
+++ rmw_cyclonedds_cpp/src/rmw_node.cpp
@@ -751,6 +751,7 @@ extern "C" rmw_ret_t rmw_event_set_callback(
       {
         return RMW_RET_INVALID_ARGUMENT;
       }
+
   }
   return RMW_RET_OK;
 }
@@ -4063,12 +4064,11 @@ extern "C" rmw_ret_t rmw_take_event(
         return RMW_RET_OK;
       }
 
-    case RMW_EVENT_INVALID: {
+    case RMW_EVENT_INVALID:
+    case RMW_EVENT_TYPE_MAX: {
         break;
       }
 
-    default:
-      rmw_cyclonedds_cpp::unreachable();
   }
   *taken = false;
   return RMW_RET_ERROR;
